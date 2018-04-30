26625d4787d22c2c85926fca23ec1eedb5e1f67a
Luis Viruena <luis@motif.com>
2018-04-30 14:07:53 -0400

    Implemented ViewPager as UI for different Animation samples.

--- app/src/main/java/com/outerspace/animatedwolf/MainActivity.java
+++ app/src/main/java/com/outerspace/animatedwolf/MainActivity.java
@@ -1,9 +1,13 @@
 package com.outerspace.animatedwolf;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -32,16 +36,55 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        loadNextFragment();
+        WolfPageAdapter adapter = new WolfPageAdapter(
+                getSupportFragmentManager(),
+                fragments);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.dialog_quit_title)
+                .setMessage(R.string.dialog_quit_message)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                         MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .create()
+                .show();
     }
 
     public void loadNextFragment() {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.fragment_frame, fragments[idxFragment]);
-        transaction.commit();
-
-        if(++idxFragment >= fragments.length)
-            idxFragment = 0;
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        int page = pager.getCurrentItem();
+        page++;
+        if(page >= fragments.length)
+            page = 0;
+        pager.setCurrentItem(page);
     }
+
+    public class WolfPageAdapter extends FragmentPagerAdapter {
+        private Fragment[] fragments;
+
+        public WolfPageAdapter(FragmentManager fm, Fragment[] fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+    }
+
 }
